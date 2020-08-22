45f876219ea41aee75b550a9e8a18dfa44b78185
camacho_isaac <isaaccamachoocampo@gmail.com>
2020-08-22 00:36:29 -0300

    trabajos de algo2

---
+++ 7540_Algo2/tp0/tp0.c
@@ -0,0 +1,37 @@
+#include "tp0.h"
+
+/* *****************************************************************
+ *                     FUNCIONES A COMPLETAR                       *
+ *         (ver en tp0.h la documentación de cada función)         *
+ * *****************************************************************/
+
+void swap (int *x, int *y) {
+
+    int aux = *x;
+
+    *x = *y;
+    *y = aux;
+}
+
+
+int maximo(int vector[], int n) {
+    if (vector == NULL)
+        return -1;
+    
+    int max = 0;
+    for(int i=0; i < n; i++){
+        if(vector[max] < vector[i])
+            max = i;
+    }
+
+    return max;
+}
+
+
+int comparar(int vector1[], int n1, int vector2[], int n2) {
+    return 0;
+}
+
+
+void seleccion(int vector[], int n) {
+}
